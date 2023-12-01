99341917530b636b2ed0b436943b0fe194f6f4f2
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-12-01 22:30:11 +0900

    fix bugs

--- sg_speech2image.py
+++ sg_speech2image.py
@@ -1,19 +1,22 @@
 import PySimpleGUI as sg
 
 sg.theme('white')
+frame1 = sg.Frame(
+    [], size=(640, 320)
+)
+frame2 = sg.Frame(
+    [], size=(640, 320)
+)
+
 layout = [ 
-    [sg.Text('Theme Browser')],
-    [sg.Text('Clicka Theme Color')],
-    [sg.Listbox(values=sg.theme_list(), size=(20, 12), key='-LIST-', enable_events=True)],
-    [sg.Button('Exit')]
+    [frame1],
+    [frame2]
 ]
-window = sg.Window('Theme Browser', layout)
+window = sg.Window('サンプル', layout)
 
 while True:
     event, values = window.read()
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
-    sg.theme(values['-LIST-'][0])
-    sg.popup_get_text('This is {}'.format(values['-LIST-'][0]))
 
 window.close()
