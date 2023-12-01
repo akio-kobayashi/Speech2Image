f2b299b0453e6db9e93dc01fc37a03caccd24e4c
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-12-01 22:14:45 +0900

    fix bugs

--- sg_speech2image.py
+++ sg_speech2image.py
@@ -7,7 +7,7 @@ layout = [
     [sg.Listbox(values=sg.theme_list(), size=(20, 12), key='-LIST-', enable_events=True)],
     [sg.Button('Exit')]
 ]
-window = sg.Window('Theme Browser', layour)
+window = sg.Window('Theme Browser', layout)
 
 while True:
     event, values = window.read()
