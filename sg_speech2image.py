8de6ce389c5957dc7e4d709874081e7b1325bc59
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2023-12-01 22:59:33 +0900

    fix bugs

--- sg_speech2image.py
+++ sg_speech2image.py
@@ -1,22 +1,37 @@
 import PySimpleGUI as sg
+from PIL import Image, ImageTk
+
+canvas_width=640
+canvas_height=480
+
+def get_image_from_file(image_file):
+    img = Image.open(image_file)
+    img = img.resize(( int(img.width * (canvas_width/img.width)), int(img.height * (canvas_height/img.width)) ))
+    img = ImageTk.PhotoImage(img)
+
+blank_image = 'blank.png'
+image_elem = sg.Image(data=get_image_from_file(blank_image))
 
 sg.theme('white')
 frame1 = sg.Frame(
     '', [], size=(640, 320)
 )
 frame2 = sg.Frame(
-    '', [], size=(640, 320)
+    '', 
+    [ image_elem ], 
+    size=(canvas_width, canvas_height)
 )
 
 layout = [ 
     [frame1],
     [frame2]
 ]
-window = sg.Window('サンプル', layout)
+window = sg.Window('サンプル', layout, resizable=True)
 
 while True:
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Exit'):
+    if event is None:
+        print('exit')
         break
 
 window.close()
